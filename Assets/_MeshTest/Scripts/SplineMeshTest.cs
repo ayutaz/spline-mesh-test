b0e1b36f4c23a3681f2bde20c7ef369bad3e431c
KOGA Mitsuhiro <shiena.jp@gmail.com>
2022-12-20 23:49:26 +0900

    feat: refactor

--- Assets/_MeshTest/Scripts/SplineMeshTest.cs
+++ Assets/_MeshTest/Scripts/SplineMeshTest.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Linq;
-using Cysharp.Threading.Tasks;
 using UniRx;
+using UniRx.Diagnostics;
 using Unity.Mathematics;
 using Unity.Splines.Examples;
 using UnityEngine;
@@ -12,26 +12,25 @@ namespace _MeshTest.Scripts
     public class SplineMeshTest : MonoBehaviour
     {
         [SerializeField] private SplineContainer splineContainer;
-        private BezierKnot LastKnot => splineContainer.Splines[0].Knots.Last();
+        private Spline Spline => splineContainer.Spline;
+        private BezierKnot LastKnot => Spline.Knots.Last();
         private MultipleRoadBehaviour _multipleRoadBehaviour;
-        private readonly float3 _targetIn = new float3(0, 0, -1);
-        private readonly float3 _targetOut = new float3(0, 0, 1);
+        private readonly float3 _targetIn = new(0, 0, -1);
+        private readonly float3 _targetOut = new(0, 0, 1);
 
         private void Start()
         {
             _multipleRoadBehaviour = GetComponent<MultipleRoadBehaviour>();
             Observable.Interval(TimeSpan.FromSeconds(3f))
-                .Subscribe(_ =>
-                {
-                    Debug.Log("Add spline's Knot in runtime");
-                    AddKnot();
-                }).AddTo(this);
+                .Debug()
+                .Subscribe(_ => { AddKnot(); }).AddTo(this);
         }
 
         private void AddKnot()
         {
-            var addKnot = new BezierKnot(new float3(0, 0, LastKnot.Position.z + 3), _targetIn, _targetOut, quaternion.identity);
-            splineContainer.Splines[0].Add(addKnot);
+            var addKnot = new BezierKnot(new float3(0, 0, LastKnot.Position.z + 3),
+                _targetIn, _targetOut, quaternion.identity);
+            Spline.Add(addKnot);
             _multipleRoadBehaviour.CreateRoads();
         }
     }
